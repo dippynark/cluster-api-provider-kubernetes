a9e69c76f195704035cff4a910183c28d8e972ff
Luke Addison <lukeaddison785@gmail.com>
2020-01-02 14:22:46 +0000

    Remove unrequired import

--- api/v1alpha1/kubernetesmachine_types.go
+++ api/v1alpha1/kubernetesmachine_types.go
@@ -19,7 +19,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
-	"sigs.k8s.io/cluster-api/errors"
 	capierrors "sigs.k8s.io/cluster-api/errors"
 )
 
@@ -67,7 +66,7 @@ type KubernetesMachineStatus struct {
 	// can be added as events to the Machine object and/or logged in the
 	// controller's output.
 	// +optional
-	FailureReason *errors.MachineStatusError `json:"failureReason,omitempty"`
+	FailureReason *capierrors.MachineStatusError `json:"failureReason,omitempty"`
 
 	// FailureMessage will be set in the event that there is a terminal problem
 	// reconciling the Machine and will contain a more verbose string suitable
